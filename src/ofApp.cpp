cb4e7ed48ef759a1cad69f4baab21af0e2568c64
Dimitri Belopopsky <dimitri@belopopsky.com>
2014-09-18 19:48:43 +0200

    Moving camera to find right angle

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,287 +1,293 @@
 #include "ofApp.h"
 
-
+#ifndef round
+#define round(x) (x<0?ceil((x)-0.5):floor((x)+0.5))
+#endif
 
 //--------------------------------------------------------------
 void ofApp::setup() {
-	ofSetLogLevel(OF_LOG_VERBOSE);
-
-	// enable depth->video image calibration
-	kinect.setRegistration(true);
-
-	kinect.init();
-	//kinect.init(true); // shows infrared instead of RGB video image
-	//kinect.init(false, false); // disable video image (faster fps)
-
-	kinect.open();		// opens first available kinect
-	//kinect.open(1);	// open a kinect by id, starting with 0 (sorted by serial # lexicographically))
-	//kinect.open("A00362A08602047A");	// open a kinect using it's unique serial #
-
-	// print the intrinsic IR sensor values
-	if(kinect.isConnected()) {
-		ofLogNotice() << "sensor-emitter dist: " << kinect.getSensorEmitterDistance() << "cm";
-		ofLogNotice() << "sensor-camera dist:  " << kinect.getSensorCameraDistance() << "cm";
-		ofLogNotice() << "zero plane pixel size: " << kinect.getZeroPlanePixelSize() << "mm";
-		ofLogNotice() << "zero plane dist: " << kinect.getZeroPlaneDistance() << "mm";
-	}
-
-#ifdef USE_TWO_KINECTS
-	kinect2.init();
-	kinect2.open();
-#endif
-
-	colorImg.allocate(kinect.width, kinect.height);
-	grayImage.allocate(kinect.width, kinect.height);
-	grayThreshNear.allocate(kinect.width, kinect.height);
-	grayThreshFar.allocate(kinect.width, kinect.height);
-
-	nearThreshold = 230;
-	farThreshold = 70;
-	bThreshWithOpenCV = true;
-
 	ofSetFrameRate(60);
+	ofSetVerticalSync(true);
+	ofBackground( 10, 10, 10);
+	
+	cameraX = 1.f;
+	cameraY = -1.f;
+	cameraZ = 1.f;
 
-	// zero the tilt on startup
-	angle = 0;
-	kinect.setCameraTiltAngle(angle);
+	cameraAngle = 0;
+	angleX = 0;
+	angleY = 0;
+	angleZ = 0;
 
-	// start from the front
-	bDrawPointCloud = false;
+	angleUpdate = false;
+
+	camera.setPosition(ofVec3f(cameraX, cameraY, cameraZ));
+	camera.lookAt(ofVec3f(0, 0, 0), ofVec3f(0, -1, 0));
+	
+	world.setup();
+
+	
+	world.setCamera(&camera);
+	
+	ground = new ofxBulletBox();
+	ground->create( world.world, ofVec3f(0., 5., 0.), 0., 10, 1.f, 10.f );
+	ground->setProperties(.25, .95);
+	ground->add();
+	
+	
+	colors[0] = ofColor(15,197,138);
+	colors[1] = ofColor(220, 0, 220);
+	colors[2] = ofColor(220, 180, 60);
+	colors[3] = ofColor(255, 20, 50);
+	
+	jointLength = .35f;
+	
+	shapes.push_back( new ofxBulletSphere() );
+	((ofxBulletSphere*)shapes[0])->create( world.world, ofVec3f(0, 2, 0), 0.4f, .5f );
+	shapes[0]->add();
+	
+	shapeColors.push_back( colors[(int)round(ofRandom(0, 3))] );
+	
+	joints.push_back( new ofxBulletJoint() );
+	joints[joints.size()-1]->create( world.world, shapes[0], ofVec3f(0, 0, 0));
+	joints[joints.size()-1]->add();
+	
+	bDrawDebug	= false;
+	bSpacebar	= false;
+	bShapesNeedErase = false;
+	
+	ofHideCursor();
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
-
-	ofBackground(100, 100, 100);
-
-	kinect.update();
-
-	// there is a new frame and we are connected
-	if(kinect.isFrameNew()) {
-
-		// load grayscale depth image from the kinect source
-		grayImage.setFromPixels(kinect.getDepthPixels(), kinect.width, kinect.height);
-
-		// we do two thresholds - one for the far plane and one for the near plane
-		// we then do a cvAnd to get the pixels which are a union of the two thresholds
-		if(bThreshWithOpenCV) {
-			grayThreshNear = grayImage;
-			grayThreshFar = grayImage;
-			grayThreshNear.threshold(nearThreshold, true);
-			grayThreshFar.threshold(farThreshold);
-			cvAnd(grayThreshNear.getCvImage(), grayThreshFar.getCvImage(), grayImage.getCvImage(), NULL);
-		} else {
-
-			// or we do it ourselves - show people how they can work with the pixels
-			unsigned char * pix = grayImage.getPixels();
-
-			int numPixels = grayImage.getWidth() * grayImage.getHeight();
-			for(int i = 0; i < numPixels; i++) {
-				if(pix[i] < nearThreshold && pix[i] > farThreshold) {
-					pix[i] = 255;
-				} else {
-					pix[i] = 0;
-				}
-			}
+	world.update();
+	ofSetWindowTitle(ofToString(ofGetFrameRate(), 0));
+	
+	mousePos = camera.screenToWorld( ofVec3f((float)ofGetMouseX(), (float)ofGetMouseY(), 0) );
+	joints[0]->updatePivotPos( mousePos, 2.f );
+	
+	if(bSpacebar) {
+		for (int i = 1; i < joints.size(); i++) {
+			delete joints[i];
 		}
-
-		// update the cv images
-		grayImage.flagImageChanged();
-
-		// find contours which are between the size of 20 pixels and 1/3 the w*h pixels.
-		// also, find holes is set to true so we will get interior contours as well....
-		contourFinder.findContours(grayImage, 10, (kinect.width*kinect.height)/2, 20, false);
+		joints.erase(joints.begin()+1, joints.end());
+		bSpacebar = false;
+		bShapesNeedErase = true;
+	}
+	
+	for (int i = 1; i < joints.size(); i++) {
+		if(i == 1) {
+			joints[i]->updatePivotPos( shapes[i-1]->getPosition(), jointLength*2.f );
+		} else {
+			joints[i]->updatePivotPos( shapes[i-1]->getPosition(), jointLength );
+		}
 	}
 
-#ifdef USE_TWO_KINECTS
-	kinect2.update();
-#endif
+	camera.setPosition(ofVec3f(cameraX,cameraY, cameraZ));
+	if (angleUpdate == true){
+		camera.rotate(cameraAngle, angleX, angleX, angleZ);
+		angleUpdate = 0;
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::draw() {
+	ofDrawAxis(10.f);
 
+
+	glEnable( GL_DEPTH_TEST );
+	camera.begin();
+
+	ofDrawAxis(10.f);
+	
+	ofSetLineWidth(1.f);
+	if(bDrawDebug) world.drawDebug();
+	
 	ofSetColor(255, 255, 255);
-
-	if(bDrawPointCloud) {
-		easyCam.begin();
-		drawPointCloud();
-		easyCam.end();
-	} else {
-		// draw from the live kinect
-		kinect.drawDepth(10, 10, 400, 300);
-		kinect.draw(420, 10, 400, 300);
-
-		grayImage.draw(10, 320, 400, 300);
-		contourFinder.draw(10, 320, 400, 300);
-
-#ifdef USE_TWO_KINECTS
-		kinect2.draw(420, 320, 400, 300);
-#endif
+	ofSphere(mousePos, .15f);
+	
+	ofEnableLighting();
+	light.enable();
+	light.setPosition( mousePos );
+	
+	ofSetColor(100, 100, 100);
+	ground->draw();
+	
+	ofSetColor(255, 0, 255);
+	for(int i = 0; i < shapes.size(); i++) {
+		ofSetColor(shapeColors[i]);
+		shapes[i]->draw();
 	}
-
-	// draw instructions
+	ofSetColor(220, 220, 220);
+	for(int i = 0; i < joints.size(); i++) {
+		joints[i]->draw();
+	}
+	light.disable();
+	ofDisableLighting();
+	
+	camera.end();
+	glDisable(GL_DEPTH_TEST);
+	
 	ofSetColor(255, 255, 255);
-	stringstream reportStream;
+	stringstream ss;
+	ss << "framerate: " << ofToString(ofGetFrameRate(),0) << endl;
+	ss << "num shapes: " << (shapes.size()) << endl;
+	ss << "draw debug (d): " << ofToString(bDrawDebug, 0) << endl;
+	ss << "break joints with spacebar: " << bSpacebar << endl;
+	ss << "Camera " << endl;
+	ss << "X: " << cameraX << " Y: " << cameraY << " Z: " << cameraZ << endl;
+	ss << "Angle: " << cameraAngle << " x: " << angleX << " y: " << angleY << " z: " << angleZ << endl;
+	ofDrawBitmapString(ss.str().c_str(), 10, 10);
 
-    if(kinect.hasAccelControl()) {
-        reportStream << "accel is: " << ofToString(kinect.getMksAccel().x, 2) << " / "
-        << ofToString(kinect.getMksAccel().y, 2) << " / "
-        << ofToString(kinect.getMksAccel().z, 2) << endl;
-    } else {
-        reportStream << "Note: this is a newer Xbox Kinect or Kinect For Windows device," << endl
-		<< "motor / led / accel controls are not currently supported" << endl << endl;
-    }
-
-	reportStream << "press p to switch between images and point cloud, rotate the point cloud with the mouse" << endl
-	<< "using opencv threshold = " << bThreshWithOpenCV <<" (press spacebar)" << endl
-	<< "set near threshold " << nearThreshold << " (press: + -)" << endl
-	<< "set far threshold " << farThreshold << " (press: < >) num blobs found " << contourFinder.nBlobs
-	<< ", fps: " << ofGetFrameRate() << endl
-	<< "press c to close the connection and o to open it again, connection is: " << kinect.isConnected() << endl;
-
-    if(kinect.hasCamTiltControl()) {
-    	reportStream << "press UP and DOWN to change the tilt angle: " << angle << " degrees" << endl
-        << "press 1-5 & 0 to change the led mode" << endl;
-    }
-
-	ofDrawBitmapString(reportStream.str(), 20, 652);
-
+	
 }
 
-void ofApp::drawPointCloud() {
-	int w = 640;
-	int h = 480;
-	ofMesh mesh;
-	mesh.setMode(OF_PRIMITIVE_POINTS);
-	int step = 2;
-	for(int y = 0; y < h; y += step) {
-		for(int x = 0; x < w; x += step) {
-			if(kinect.getDistanceAt(x, y) > 0) {
-				mesh.addColor(kinect.getColorAt(x,y));
-				mesh.addVertex(kinect.getWorldCoordinateAt(x, y));
-			}
-		}
-	}
-	glPointSize(3);
-	ofPushMatrix();
-	// the projected points are 'upside down' and 'backwards'
-	ofScale(1, -1, -1);
-	ofTranslate(0, 0, -1000); // center the points a bit
-	ofEnableDepthTest();
-	mesh.drawVertices();
-	ofDisableDepthTest();
-	ofPopMatrix();
-}
+
 
 //--------------------------------------------------------------
-void ofApp::exit() {
-	kinect.setCameraTiltAngle(0); // zero the tilt on exit
-	kinect.close();
-
-#ifdef USE_TWO_KINECTS
-	kinect2.close();
-#endif
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed (int key) {
+void ofApp::keyPressed(int key) {
 	switch (key) {
 		case ' ':
-			bThreshWithOpenCV = !bThreshWithOpenCV;
+			bSpacebar = true;
+			break;
+		case 'd':
+			bDrawDebug = !bDrawDebug;
+			break;
+		case 356:
+			cameraZ += 1;
+			break;
+		case 357:
+			cameraX -= 1;
+			break;
+		case 358:
+			cameraZ -= 1;
+			break;
+		case 359:
+			cameraX += 1;
+			break;
+		case 'e':
+			cameraY -= 1;
+			break;
+		case 'r':
+			cameraY += 1;
 			break;
 
-		case'p':
-			bDrawPointCloud = !bDrawPointCloud;
+		case 'u':
+			cameraAngle += 1;
+			break;
+		case 'i':
+			cameraAngle -= 1;
 			break;
 
-		case '>':
-		case '.':
-			farThreshold ++;
-			if (farThreshold > 255) farThreshold = 255;
+		case 'j':
+			if (angleX == 1){
+				angleX = 0;
+			} else {
+				angleX = 1;
+			}
+			angleUpdate = true;
 			break;
 
-		case '<':
-		case ',':
-			farThreshold --;
-			if (farThreshold < 0) farThreshold = 0;
+		case 'k':
+			if (angleY == 1){
+				angleY = 0;
+			} else {
+				angleY = 1;
+			}
+			angleUpdate = true;
 			break;
 
-		case '+':
-		case '=':
-			nearThreshold ++;
-			if (nearThreshold > 255) nearThreshold = 255;
+
+		case 'l':
+			if (angleZ == 1){
+				angleZ = 0;
+			} else {
+				angleZ = 1;
+			}
+			angleUpdate = true;
 			break;
 
-		case '-':
-			nearThreshold --;
-			if (nearThreshold < 0) nearThreshold = 0;
-			break;
 
-		case 'w':
-			kinect.enableDepthNearValueWhite(!kinect.isDepthNearValueWhite());
-			break;
+			
 
-		case 'o':
-			kinect.setCameraTiltAngle(angle); // go back to prev tilt
-			kinect.open();
-			break;
-
-		case 'c':
-			kinect.setCameraTiltAngle(0); // zero the tilt
-			kinect.close();
-			break;
-
-		case '1':
-			kinect.setLed(ofxKinect::LED_GREEN);
-			break;
-
-		case '2':
-			kinect.setLed(ofxKinect::LED_YELLOW);
-			break;
-
-		case '3':
-			kinect.setLed(ofxKinect::LED_RED);
-			break;
-
-		case '4':
-			kinect.setLed(ofxKinect::LED_BLINK_GREEN);
-			break;
-
-		case '5':
-			kinect.setLed(ofxKinect::LED_BLINK_YELLOW_RED);
-			break;
-
-		case '0':
-			kinect.setLed(ofxKinect::LED_OFF);
-			break;
-
-		case OF_KEY_UP:
-			angle++;
-			if(angle>30) angle=30;
-			kinect.setCameraTiltAngle(angle);
-			break;
-
-		case OF_KEY_DOWN:
-			angle--;
-			if(angle<-30) angle=-30;
-			kinect.setCameraTiltAngle(angle);
+			
+		default:
+			cout << "keyPressed " << (char)key << " " << key << endl;
 			break;
 	}
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button)
-{}
+void ofApp::keyReleased(int key) {
+	
+}
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button)
-{}
+void ofApp::mouseMoved(int x, int y) {
+	
+}
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button)
-{}
+void ofApp::mouseDragged(int x, int y, int button) {
+	
+}
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h)
-{}
+void ofApp::mousePressed(int x, int y, int button) {
+	if(bShapesNeedErase) {
+		for (int i = 1; i < shapes.size(); i++) {
+			delete shapes[i];
+		}
+		shapes.erase( shapes.begin()+1, shapes.end() );
+		bShapesNeedErase = false;
+		shapeColors.erase(shapeColors.begin()+1, shapeColors.end());
+	}
+	
+	float rsize = ofRandom(.15, .3);
+	
+	ofVec3f diff;
+	if(shapes.size() < 2) {
+		diff = mousePos - shapes[0]->getPosition();
+	} else {
+		diff = shapes[shapes.size()-2]->getPosition() - shapes[shapes.size()-1]->getPosition();
+	}
+	diff.normalize();
+	if(shapes.size() < 2) {
+		diff *= -(jointLength*2.f);
+	} else {
+		diff *= -jointLength;
+	}
+	diff += shapes[shapes.size()-1]->getPosition();
+	
+	shapes.push_back( new ofxBulletSphere() );
+	((ofxBulletSphere*)shapes[shapes.size()-1])->create( world.world, diff, .8, rsize );
+	shapes[shapes.size()-1]->add();
+	
+	shapeColors.push_back( colors[(int)round(ofRandom(0, 3))] );
+	
+	
+	joints.push_back( new ofxBulletJoint() );
+	joints[joints.size()-1]->create(world.world, shapes[shapes.size()-1], shapes[shapes.size()-2]->getPosition());
+	joints[joints.size()-1]->add();
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) { 
+	
+}
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,53 +1,46 @@
 #pragma once
 
 #include "ofMain.h"
-
-#include "ofxKinect.h"
-#include "ofxOpenCv.h"
 #include "ofxBullet.h"
-#include "ofxOpenNI2.h"
-
-#define USE_TWO_KINECTS
 
 class ofApp : public ofBaseApp{
 
 public:
-	
 	void setup();
 	void update();
 	void draw();
-	void exit();
-	
-	void drawPointCloud();
-	
-	void keyPressed(int key);
+
+	void keyPressed  (int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y );
 	void mouseDragged(int x, int y, int button);
 	void mousePressed(int x, int y, int button);
 	void mouseReleased(int x, int y, int button);
 	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
 	
-	ofxKinect kinect;
+	ofxBulletWorldRigid			world;
+	ofxBulletBox*				ground;
 	
-#ifdef USE_TWO_KINECTS
-	ofxKinect kinect2;
-#endif
+	vector<ofxBulletBaseShape*> shapes;
+	vector<ofxBulletJoint*>		joints;
+	vector<ofColor>				shapeColors;
+	ofColor						colors[4];
 	
-	ofxCvColorImage colorImg;
+	bool						bDrawDebug;
+	bool						bSpacebar;
+	bool						bShapesNeedErase;
 	
-	ofxCvGrayscaleImage grayImage; // grayscale depth image
-	ofxCvGrayscaleImage grayThreshNear; // the near thresholded image
-	ofxCvGrayscaleImage grayThreshFar; // the far thresholded image
+	float						jointLength;
 	
-	ofxCvContourFinder contourFinder;
-	
-	bool bThreshWithOpenCV;
-	bool bDrawPointCloud;
-	
-	int nearThreshold;
-	int farThreshold;
-	
-	int angle;
-	
-	// used for viewing the point cloud
-	ofEasyCam easyCam;
+	ofCamera					camera;
+	ofLight						light;
+	ofVec3f						mousePos;
+
+	float cameraX, cameraY, cameraZ;
+	float cameraAngle;
+	int angleX, angleY, angleZ;
+	bool angleUpdate;
+		
 };
